aabadb288a4e4c9d5179d3b3a2c94c290b205ef3
alwandp <alwandp34@gmail.com>
2022-02-16 18:21:56 +0700

    Menambahkan file storage.js dan menerapkan localstorage untuk WebCalculator

--- assets/script.js
+++ assets/script.js
@@ -43,13 +43,28 @@ function handleOperator(operator) {
 
 /* Fungsi Calculation */
 function performCalculation() {
+  if(calculator.firstNumber == null || calculator.operator == null) {
+    alert("Anda belum menetapkan operator");
+    return;
+  }
+
+  let result = 0;
   if(calculator.operator === "+") {
     result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
   } else {
     result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
   }
 
+  // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+  const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result
+  }
+  putHistory(history);
   calculator.displayNumber = result;
+  renderHistory();
 }
 
 /* Fungsi untuk memasukkan angka */ 
